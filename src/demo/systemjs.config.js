2a37259e2e8f8ad34c8684103800978058e5658e
Roman Lyubimov <lubimov.nsk@gmail.com>
2017-08-09 00:12:17 +0700

    Change lib name

--- src/demo/systemjs.config.js
+++ src/demo/systemjs.config.js
@@ -39,7 +39,7 @@
       rxjs: {
         defaultExtension: 'js'
       },
-      'quickstart-lib': {
+      'ng-selectable': {
         main: 'index.js',
         defaultExtension: 'js',
         meta: {
