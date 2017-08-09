f98a2299d66d972dbc824a24881a6d89f880b378
Roman Lyubimov <lubimov.nsk@gmail.com>
2017-08-10 00:50:27 +0700

    Add value accessor to simple select

--- src/lib/src/simple-select/component/simple-select.component.ts
+++ src/lib/src/simple-select/component/simple-select.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+const SIMPLE_SELECT_VALUE_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => SimpleSelectComponent),
+  multi: true,
+};
 
 @Component({
   selector: 'ng-simple-select',
   templateUrl: './simple-select.component.html',
-  styleUrls: ['./simple-select.component.css']
+  styleUrls: ['./simple-select.component.css'],
+  providers: [SIMPLE_SELECT_VALUE_ACCESSOR],
 })
 export class SimpleSelectComponent {
+  private innerValue: any;
+  private onChange = (_: any) => {};
+  private onTouched = () => {};
 
+  get value(): any {
+    return this.innerValue;
+  }
+
+  set value(value: any) {
+    if (this.innerValue !== value) {
+      this.innerValue = value;
+      this.onChange(value);
+    }
+  }
+
+  writeValue(value: any) {
+    if (this.innerValue !== value) {
+      this.innerValue = value;
+    }
+  }
+
+  registerOnChange(fn: (value: any) => void) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void) {
+    this.onTouched = fn;
+  }
 }
