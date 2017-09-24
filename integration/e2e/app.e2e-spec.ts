a68e8cc973d7f74426326ca5efeab3a606f10f53
Roman Lyubimov <lubimov.nsk@gmail.com>
2017-09-24 18:39:39 +0700

    Add procrator in dev deps

--- integration/e2e/app.e2e-spec.ts
+++ integration/e2e/app.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { browser, element, by } from 'protractor';
+import { browser } from 'protractor';
 
 describe('QuickStart Lib E2E Tests', function () {
 
@@ -9,13 +9,4 @@ describe('QuickStart Lib E2E Tests', function () {
       expect(browserLog).toEqual([]);
     });
   });
-
-  it('should display lib', () => {
-    expect(element(by.css('h2')).getText()).toEqual('Hello Angular Library');
-  });
-
-  it('should display meaning', () => {
-    expect(element(by.css('h3')).getText()).toEqual('Meaning is: 42');
-  });
-
 });
